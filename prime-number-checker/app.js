b058673c2eb9ceeb2d631d199fa242b55b94839c
PrimordialOrigin <RamerafheKaone@protonmail.com>
2024-01-03 10:17:48 +0200

    fix the checker

--- prime-number-checker/app.js
+++ prime-number-checker/app.js
@@ -6,27 +6,22 @@ function checkPrimeNum(event){
 
     event.preventDefault();
 
-    let ourValue = document.getElementById("number").value;
+    const ourValue = document.getElementById("number").value;
     const ourDiv = document.getElementsByClassName("formbox")[0];
-    let newDiv = document.createElement("div");
+    const newDiv = document.createElement("div");
 
-    
-
-    if(ourValue < 2){
-        newDiv.innerHTML= "Not a prime number";
-        
+    if(isNaN(Number(ourValue))){
+        newDiv.innerHTML= "Enter a number!";
+    } else if(ourValue < 2){
+        newDiv.innerHTML= "Not a prime number";        
     } else if (ourValue % 2 === 0 || ourValue % 3 == 0){
         newDiv.innerHTML = "Not a prime number";
-       
     } else{
-        newDiv.innerHTML = "It is a prime number";
-        
+        newDiv.innerHTML = "It is a prime number"; 
     }
 
     ourDiv.appendChild(newDiv);
 
 }
-
-
 
 getBtn.addEventListener("click", checkPrimeNum)
